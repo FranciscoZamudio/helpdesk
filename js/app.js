fd0b8f6c11cdb7aff3e20461f834ab3c9e244c7c
FranciscoZamudio <fj_darkthunder@hotmail.com>
2018-08-08 00:10:23 -0500

    basic frontend structure (view and router)

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//creamos el modulo y le aplicamos la configuración
+//var moduleApp = angular.module("moduleApp", ["ngRoute"]).config(configRoutes);
+var moduleApp = angular.module('moduleApp', ['ui.router']);
+
+moduleApp.config(function ($stateProvider,$urlRouterProvider) {
+
+    var ticketsState = {
+        name: 'tickets',
+        url: '/tickets',
+        templateUrl: 'views/tickets.html'
+    }
+
+    var dashboardState = {
+        name: 'dashboard',
+        url: '/dashboard',
+        templateUrl: 'views/dashboard.html'
+    }
+    var metricsState = {
+        name: 'metrics',
+        url: '/metrics',
+        template: "<h1>About Me</h1><p>I'm a web developer and entrepreneur with 3+ years of experience</p>"
+    }
+
+
+    $stateProvider.state(ticketsState);
+    $stateProvider.state(dashboardState);
+    $stateProvider.state(metricsState);
+    $urlRouterProvider.otherwise('/tickets');
+
+});
